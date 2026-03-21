dc936f93ce2046ad294583c5f5787c843059f7fb
zndr3 <zandersison18@gmail.com>
2026-03-21 23:26:00 +0800

    added manage account

--- simple_bank.py
+++ simple_bank.py
@@ -1,3 +1,4 @@
+
 import sqlite3
 import hashlib
 import os
@@ -79,26 +80,45 @@ class Bank:
     def check_account(self, user_id):
         self.cursor.execute("SELECT * FROM accounts WHERE user_id=?", (user_id,))
         return self.cursor.fetchone()
+
+    def add_account(self, user_id, acc_type, balance, acc_num):
+        self.cursor.execute("INSERT INTO accounts (user_id, acc_type, balance, acc_num) VALUES (?, ?, ?, ?)", (user_id, acc_type, 0.00, acc_num))
+        self.conn.commit()
     
     def create_account(self, user_id):
-        acc_type = input("1. Create Checking Account, enter (c)\n"
-                        "2. Create Savings Account, enter (s)\n"
-                        "Enter your choice: ").lower()
-        if acc_type == 'c':
-            acc_type = "Checking"
-        elif acc_type == 's':
-            acc_type = "Savings"
-        else:
-            print("Invalid option. Please choose 'c' or 's'.")
-        # self.dashboard(user_id)
-        if self.check_account(user_id):
-            acc_num = self.get_acc_num(user_id)[0] + 1
-            self.cursor.execute("INSERT INTO accounts (user_id, acc_type, balance, acc_num) VALUES (?, ?, ?, ?)", (user_id, acc_type, 0.00, acc_num))
-            self.conn.commit()
-        else:
-            acc_num = 1
-            self.cursor.execute("INSERT INTO accounts (user_id, acc_type, balance, acc_num) VALUES (?, ?, ?, ?)", (user_id, acc_type, 0.00, acc_num))
-            self.conn.commit()
+        acc_type = ""
+        while acc_type not in ['1', '2']:
+            acc_type = input("1. Create Checking Account\n"
+                            "2. Create Savings Account\n"
+                            "Enter Zero '0' to exit\n"
+                            "Enter your choice number: ")
+            if acc_type == '0':
+                return
+
+            elif acc_type == '1':
+                acc_type = "Checking"
+                if self.check_account(user_id):
+                    acc_num = self.get_acc_num(user_id)[0] + 1
+                    self.add_account(user_id, acc_type, 0.00, acc_num)
+                    return
+                else:
+                    acc_num = 1
+                    self.add_account(user_id, acc_type, 0.00, acc_num)
+                    return
+            elif acc_type == '2':
+                acc_type = "Savings"
+                if self.check_account(user_id):
+                    acc_num = self.get_acc_num(user_id)[0] + 1
+                    self.add_account(user_id, acc_type, 0.00, acc_num)
+                    return
+                else:
+                    acc_num = 1
+                    self.add_account(user_id, acc_type, 0.00, acc_num)
+                    return
+            else:
+                print("Invalid option. Please choose '1' '2' or '0'.")
+        
+        
 
 
     def get_acc_num(self, user_id):
@@ -106,19 +126,29 @@ class Bank:
         return self.cursor.fetchone()
         
     def close_acc_num(self, user_id):
-        # print(self.get_acc_nums(user_id))
+
+        print("Close Account")
         acc_nums = [acc[0] for acc in self.get_acc_nums(user_id)]
-        acc_num = int(input("Enter account number to close: "))
-        if acc_num in acc_nums:
-            if not self.get_balance(user_id, acc_num):
-                self.cursor.execute("DELETE FROM accounts WHERE user_id=? AND acc_num=?", (user_id, acc_num))
-                self.conn.commit()
-            else:
-                print("Cannot close account with balance.\n")
-                print("Please choose another account number.")
-                self.close_acc_num(user_id)
-        else:
-            print("No account found with that number.")
+        acc_num = 0
+        while acc_num not in acc_nums:
+            try:
+                acc_num = int(input("Enter Zero '0' to exit\nEnter account number to close: "))
+                if acc_num == 0:
+                    break
+                elif acc_num in acc_nums:
+                    if not self.get_balance(user_id, acc_num):
+                        self.cursor.execute("DELETE FROM accounts WHERE user_id=? AND acc_num=?", (user_id, acc_num))
+                        self.conn.commit()
+                    else:
+                        print("Cannot close account with balance.")
+                        print("Please choose another account number.")
+                        acc_num = 0
+                else:
+                    print("No account found with that number.")
+
+            except ValueError:
+                print("Invalid input. Please enter a valid account number.")
+
 
     def get_balance(self, user_id, acc_num):
         self.cursor.execute("SELECT balance FROM accounts WHERE user_id=? AND acc_num=?", (user_id, acc_num))
@@ -140,6 +170,71 @@ class Bank:
         df = pd.read_sql_query(query, self.conn, params=params)
 
         print(df)
+    
+    def get_acc_id(self ,user_id, acc_num):
+        self.cursor.execute("SELECT id FROM accounts WHERE user_id=? AND acc_num=?", (user_id, acc_num))
+        return self.cursor.fetchone()[0]
+    
+    def deposit(self, acc_id, amount):
+        self.cursor.execute("UPDATE accounts SET balance = balance + ? WHERE id = ?", (amount, acc_id))
+        self.conn.commit()
+        self.record_transaction(acc_id, "Deposit", amount, f"Added {amount}")
+
+    def withdraw(self, acc_id, amount):
+        self.cursor.execute("UPDATE accounts SET balance = balance - ? WHERE id = ?", (amount, acc_id))
+        self.conn.commit()
+        self.record_transaction(acc_id, "Withdrawal", amount, f"Withdrew {amount}")
+
+    def transfer(self, user_id, acc_num, transfer_to_acc_num, amount):
+        self.cursor.execute("UPDATE accounts SET balance = balance - ? WHERE user_id = ? AND acc_num = ?", (amount, user_id, acc_num))
+        self.cursor.execute("UPDATE accounts SET balance = balance + ? WHERE user_id = ? AND acc_num = ?", (amount, user_id, transfer_to_acc_num))
+        self.conn.commit()
+        self.record_transaction(self.get_acc_id(user_id, acc_num), "Transfer" , "Withdrawal", amount, f"Account number {acc_num} transferred {amount} to account number {transfer_to_acc_num}")
+        
+    
+    def record_transaction(self, acc_id, type, amount, desc):
+        self.cursor.execute("INSERT INTO transactions (acc_id, type, amount, desc) VALUES (?, ?, ?, ?)", (acc_id, type, amount, desc))
+        self.conn.commit()
+
+
+    
+    def manage_acc(self, user_id):
+        acc_nums = [acc[0] for acc in self.get_acc_nums(user_id)]
+        acc_num = int(input("Enter account number to manage: "))
+        if acc_num in acc_nums:
+            print("1. Deposit")
+            print("2. Withdraw")
+            print("3. Transfer")
+            print("4. Back")
+            choice = input("Enter choice number: ")
+
+            if choice == '1':
+                amount = float(input("Enter amount to deposit: "))
+                self.deposit(self.get_acc_id(user_id, acc_num), amount)
+            
+            elif choice == '2':
+                amount = float(input("Enter amount to withdraw: "))
+                if self.get_balance(user_id, acc_num) < amount:
+                    print("Insufficient funds to withdraw.")
+                else:
+                    self.withdraw(self.get_acc_id(user_id, acc_num), amount)
+
+            elif choice == '3':
+                transfer_to_acc_num = int(input("Enter account number to transfer to: "))
+                if transfer_to_acc_num not in acc_nums:
+                    print("No account found with that number.")
+                elif transfer_to_acc_num == acc_num:
+                    print("Cannot transfer to the same account.")
+                else:
+                    amount = float(input("Enter amount to transfer: "))
+                    if self.get_balance(user_id, acc_num) < amount:
+                        print("Insufficient funds to transfer.")
+                    else:
+                        self.transfer(user_id, acc_num, transfer_to_acc_num, amount)
+            
+        else:
+            print("No account found with that number.")
+        
 
     def dashboard(self, user_id, username):
         # isloggedin = True
@@ -152,10 +247,11 @@ class Bank:
             self.get_accounts(user_id)
             print("1. Create Account")
             print("2. Close Account")
-            print("3. Logout")
+            print("3. Manage Account")
+            print("4. Logout")
             choice = 0
 
-            while choice not in [1, 2, 3]:
+            while choice not in [1, 2, 3, 4]:
                 choice = input("Enter choice number: ")
                 if choice == '1':
                     self.create_account(user_id)
@@ -166,21 +262,19 @@ class Bank:
                     self.dashboard(user_id, username)
 
                 elif choice == '3':
+                    self.manage_acc(user_id)
+                    self.dashboard(user_id, username)
+
+                elif choice == '4':
                     break
                 
                 else:
-                    print("Invalid option. Please choose '1' or '2'.")
+                    print("Invalid option. Please choose '1' '2' '3' or '4'.")
                 
-
-            # self.create_account(user_id)
-            # print("1. View Balance")
-            # print("2. Deposit")
-            # print("3. Withdraw")
-            # print("4. Logout")
-
         else:
             print("You don't have an account:")
             self.create_account(user_id)
+            self.dashboard(user_id, username)
                 
         
     def check_password(self, password):
@@ -221,9 +315,12 @@ class Bank:
 
     def login(self):
         print("Login")
-        while True:
-            username = input("Enter username: ")
-            if self.check_user(username):
+        username = " "
+        while username != "0":
+            username = input("Enter Zero '0' to exit\nEnter username: ")
+            if username == "0":
+                return
+            elif self.check_user(username):
                 while True:
                     password = input("Enter 6 digit PIN: ")
                     if verify_text_hash(self.get_hash(username), password):
@@ -257,8 +354,7 @@ def verify_text_hash(stored_text, provided_password):
 
 if __name__ == "__main__":
     db = Bank()
-    while True:
-        
+    while True:   
         print("Welcome to SQL Bank!")
         try:
             enter = input("Do you want to (l)ogin or (r)egister? ").lower()
