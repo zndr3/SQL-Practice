8ec1b17a3feabe8753c70016144e33859d5155d3
zndr3 <zandersison18@gmail.com>
2026-03-27 22:14:33 +0800

    added bank data

--- db.py
+++ db.py
@@ -2,23 +2,24 @@
 
 from faker import Faker
 import sqlite3
+from numpy import add
 import pandas as pd
 
 # Initialize Faker
 fake = Faker(['en_IN'])
 
 # Use 'with' to connect to the SQLite database and automatically close the connection when done
-with sqlite3.connect('my_database.db') as connection:
+with sqlite3.connect('bank_data.sqlite3') as connection:
 
     # Create a cursor object
-    cursor = connection.cursor()
+    # cursor = connection.cursor()
 
-    delete_query = '''
-    DELETE FROM Students
-    WHERE name = ?;'''
-    student_name = input("Enter the name of the student to delete: ")
-    cursor.execute(delete_query, (student_name,))
-    connection.commit()
+    # delete_query = '''
+    # DELETE FROM Students
+    # WHERE name = ?;'''
+    # student_name = input("Enter the name of the student to delete: ")
+    # cursor.execute(delete_query, (student_name,))
+    # connection.commit()
 
     # update_query = '''
     # UPDATE Students 
@@ -79,3 +80,39 @@ with sqlite3.connect('my_database.db') as connection:
     # Commit the changes        SELECT does not need commit, but if you have any INSERT, UPDATE, or DELETE operations, you should commit the changes to save them to the database.
     # connection.commit()
 
+    def delete_row(acc_num):
+        # conn = sqlite3.connect('bank_data.sqlite3')
+        cursor = connection.cursor()
+        cursor.execute("DELETE FROM accounts WHERE acc_num = ?;", (acc_num,))
+        connection.commit()
+
+    def add_col():
+        cursor = connection.cursor()
+        cursor.execute("ALTER TABLE transactions ADD COLUMN desc TEXT;")
+        connection.commit()
+
+
+    def insert_data(user_id):
+        cursor = connection.cursor()
+        cursor.execute('''UPDATE accounts 
+                            SET acc_num = 1 
+                            WHERE user_id = ?;
+                        ''', (user_id,))
+        connection.commit()
+    
+    def get_acc_num(user_id):
+        cursor = connection.cursor()
+        cursor.execute("SELECT acc_num FROM accounts WHERE user_id=? ORDER BY acc_num DESC", (user_id,))
+        print(cursor.fetchone()[0])
+
+    def get_acc_id(user_id, acc_num):
+        cursor = connection.cursor()
+        cursor.execute("SELECT id FROM accounts WHERE user_id=? AND acc_num=?", (user_id, acc_num))
+        print(cursor.fetchone()[0])
+        
+
+
+if __name__ == "__main__":
+    delete_row(4)
+    # add_col("")
+    # get_acc_id("d7499ce3-5fa7-4e29-a97e-d1c5ed8b9b68" , 4)
